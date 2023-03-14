0417f9177ebc63eadb25e8a87065c8745214467a
reigada66 <96894638+reigada66@users.noreply.github.com>
2023-03-14 13:39:47 +0000

    imagem2

--- app/src/main/java/com/example/gprod/MainActivity.java
+++ app/src/main/java/com/example/gprod/MainActivity.java
@@ -5,12 +5,17 @@ import static android.content.ContentValues.TAG;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -19,26 +24,38 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
     private EditText mTitle , mDesc;
-    private Button mSaveBtn, mShowBtn;
+    private Button mSaveBtn, mShowBtn, btnChoose, btnUpload;
+    private ImageView imageView;
+    private Uri filePath;
+
     private FirebaseFirestore db;
     private String uTitle, uDesc , uId;
+    private StorageReference storageReference;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTitle = findViewById(R.id.edit_title);
+        mTitle = findViewById(R.id.edit_nomeproduto);
         mDesc = findViewById(R.id.edit_desc);
         mSaveBtn = findViewById(R.id.save_btn);
 //        mShowBtn = findViewById(R.id.showall_btn);
+        btnChoose = (Button) findViewById(R.id.btnChooseImage);
+        btnUpload = (Button) findViewById(R.id.btnUpload);
+        imageView = (ImageView) findViewById(R.id.imgView);
 
         db= FirebaseFirestore.getInstance();
 
@@ -52,7 +69,69 @@ public class MainActivity extends AppCompatActivity {
                     saveToFireStore(id , title , desc);
              }
         });
+
+        btnChoose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                chooseImage();
+            }
+        });
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadImage();
+            }
+        });
+
     }
+
+    private void chooseImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK
+                && data != null && data.getData() != null )
+        {
+            filePath = data.getData();
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), filePath);
+                imageView.setImageBitmap(bitmap);
+            }
+            catch (IOException e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void uploadImage() {
+        if(filePath != null)
+        {
+            storageReference = FirebaseStorage.getInstance().getReference();
+
+            StorageReference ref = storageReference.child("imagens/");
+            ref.putFile(filePath)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(MainActivity.this, "Uploaded", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(MainActivity.this, "Failed "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
 
     private void updateToFireStore(String id , String title , String desc){
 
--- app/src/main/java/com/example/gprod/SignInActivity.java
+++ app/src/main/java/com/example/gprod/SignInActivity.java
@@ -1,23 +1,32 @@
 package com.example.gprod;
 
+import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.content.Intent
-import android.os.Bundle
-import android.util.Log
-import android.widget.Toast
-import androidx.activity.result.ActivityResultLauncher
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.gprod.databinding.ActivitySignInBinding;
-import com.google.firebase.auth.FirebaseAuth
-//import com.google.firebase.codelab.friendlychat.databinding.ActivitySignInBinding
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract;
+import com.firebase.ui.auth.IdpResponse;
+import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class SignInActivity extends AppCompatActivity {
 
     private ActivitySignInBinding binding;
+    private FirebaseAuth mAuth;
 
     private final ActivityResultLauncher<Intent> signInLauncher = registerForActivityResult(
             new FirebaseAuthUIActivityResultContract(),
@@ -29,77 +38,62 @@ public class SignInActivity extends AppCompatActivity {
             }
     );
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
-        signIn = registerForActivityResult(
-            registerForActivityResult(FirebaseAuthUIActivityResultContract(), this::onSignInResult)
+        binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // Initialize FirebaseAuth
+        // TODO: implement
+        mAuth = FirebaseAuth.getInstance();
 
     }
-}
 
-
-
-
-
-        override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // This codelab uses View Binding
-        // See: https://developer.android.com/topic/libraries/view-binding
-        binding = ActivitySignInBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        }
-
-public override fun onStart() {
-        super.onStart()
+    @Override
+    public void onStart() {
+        super.onStart();
 
         // If there is no signed in user, launch FirebaseUI
         // Otherwise head to MainActivity
-        if (Firebase.auth.currentUser == null) {
+        if (mAuth.getCurrentUser() == null) {
         // Sign in with FirebaseUI, see docs for more details:
         // https://firebase.google.com/docs/auth/android/firebaseui
-        val signInIntent = AuthUI.getInstance()
-        .createSignInIntentBuilder()
-        .setLogo(R.mipmap.ic_launcher)
-        .setAvailableProviders(listOf(
-        AuthUI.IdpConfig.EmailBuilder().build(),
-        AuthUI.IdpConfig.GoogleBuilder().build(),
-        ))
-        .build()
+            List<AuthUI.IdpConfig> providers = Arrays.asList(
+                    new AuthUI.IdpConfig.EmailBuilder().build(),
+                    new AuthUI.IdpConfig.GoogleBuilder().build());
 
-        signIn.launch(signInIntent)
+            Intent signInIntent = AuthUI.getInstance()
+                    .createSignInIntentBuilder()
+                    .setAvailableProviders(providers)
+                    .build();
+            signInLauncher.launch(signInIntent);
         } else {
-        goToMainActivity()
+            goToMainActivity();
         }
         }
 
-private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        if (result.resultCode == RESULT_OK) {
-        Log.d(TAG, "Sign in successful!")
-        goToMainActivity()
+    private void onSignInResult(FirebaseAuthUIAuthenticationResult result) {
+        IdpResponse response = result.getIdpResponse();
+        if (result.getResultCode() == RESULT_OK) {
+            // Successfully signed in
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            Log.d("SignInActivity", "Sign in successful!");
+            goToMainActivity();
         } else {
-        Toast.makeText(
-        this,
-        "There was an error signing in",
-        Toast.LENGTH_LONG).show()
+            Toast.makeText(this,"There was an error signing in", Toast.LENGTH_LONG).show();
+            if (response == null) {
+                Log.w("SignInActivity", "Sign in canceled");
+            } else {
+               Log.w("SignInActivity", "Sign in error", response.getError());
+            }
+        }
+    }
 
-        val response = result.idpResponse
-        if (response == null) {
-        Log.w(TAG, "Sign in canceled")
-        } else {
-        Log.w(TAG, "Sign in error", response.error)
-        }
-        }
-        }
-
-private fun goToMainActivity() {
-        startActivity(Intent(this, MainActivity::class.java))
-        finish()
+    private void goToMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
         }
 
-        companion object {
-private const val TAG = "SignInActivity"
-        }
-        }
+}
